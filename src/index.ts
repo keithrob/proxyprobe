305ccad7d0ab8ee252c4cace61fa7f03a994eae4
Keith Robertson <kerobert@microsoft.com>
2018-08-22 13:08:15 -0700

    String fixes

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,36 @@
 import * as httpm from "typed-rest-client/HttpClient";
 import * as ifm from "typed-rest-client/Interfaces";
 import * as ap from "argparse/lib/argparse";
+import chalk from "chalk"
 
 const VERSION = "1.0.0";
 
 async function probeURI(
   uri: string,
-  proxyuri: string,
-  ignoreselfsigned: boolean,
+  proxyuri?: string,
+  ignoreselfsigned = true,
   proxyUsername?: string,
   proxyPassword?: string,
-  caFile?: string
+  caFile?: string,
+  passphrase?: string
 ) {
   let userAgent: string = "proxyprobe";
-  let options: ifm.IRequestOptions = <ifm.IRequestOptions>{
-    ignoreSslError: ignoreselfsigned,
-    cert: {
-      caFile: caFile
-    },
-    proxy: {
-      proxyPassword: proxyPassword,
-      proxyUrl: proxyuri,
-      proxyUsername: proxyPassword
-    }
-  };
+  let options: ifm.IRequestOptions;
+  if (proxyuri){
+    options = <ifm.IRequestOptions>{
+      ignoreSslError: ignoreselfsigned,
+      cert: {
+        caFile: caFile,
+        passphrase: passphrase
+      },
+      proxy: {
+        proxyPassword: proxyPassword,
+        proxyUrl: proxyuri,
+        proxyUsername: proxyPassword
+      }
+    };
+  }
+
   let httpc: httpm.HttpClient = new httpm.HttpClient(userAgent, [], options);
   try {
     let res: httpm.HttpClientResponse = await httpc.get(uri);
@@ -49,8 +56,8 @@ async function probeURI(
     console.log(JSON.stringify(data, null, 4));
   } catch (error) {
     if (error.code && error.code === 'UNABLE_TO_VERIFY_LEAF_SIGNATURE'){
-      console.error(`ERROR: Your proxy is probably expressing a self signed cert.  You
-      either need to ignoreselfsined or supply a CA file that isn't self signed.`)
+      console.error(`${chalk.red.bold('ERROR:')} Your proxy is probably expressing a self signed cert.  You
+either need to ignoreselfsined or supply a CA file that isn't self signed.`)
     }
     console.error(error);
   }
@@ -68,39 +75,59 @@ attempt to egress traffic through your proxy and will print as many details
 as possible to assist with debug.`
 });
 parser.addArgument(["-p", "--proxy"], {
-  help: "proxy http://my.corporate.proxy.example.com/",
-  required: true
+  help: "The URI of your proxy server."
 });
 parser.addArgument(["-d", "--dest"], {
-  help: "dest https://httpbin.org/get (DEFAULT is https://httpbin.org/get)",
+  help: `The URI that you would like to probe through your proxy server.
+  The default is https://httpbin.org/get.`,
   defaultValue: "https://httpbin.org/get"
 });
 parser.addArgument(["-i", "--ignoreselfsigned"], {
-  help: `ignoreselfsigned (This will STOP ignoring self signed certs.  We ignore them
-  by default because it is unlikely that your proxy is expressing officially signed
-  certs.  If you set this you probably need to supply cafile depending on your cert
-  chain.)`,
+  help: `By default, this tool ignores the certificate expressed by your proxy server because
+  most corporate proxies are using self signed certificates.  Setting this flag will cause this
+  client to STOP ignoring self signed certs.  If you set this you probably need to supply
+  cafile.`,
   // See: https://github.com/nodejs/node/blob/e570ae79f5b1d74fed52d2aed7f014caaa0836dd/lib/_tls_wrap.js#L1112
   defaultValue: true,
   action: 'storeFalse'
 });
 parser.addArgument(["-u", "--user"], {
-  help: "user user@example.com"
+  help: "The user that you should authenticate to your proxy server as."
 });
 parser.addArgument(["-P", "--password"], {
   help:
-    "password secret (IMPORTANT: Please clear your shell history if you use this)"
+    "The password to your proxy server. Please clear your shell history if you use this."
 });
 parser.addArgument(["-c", "--cafile"], {
   help:
-    `cafile /path/to/cafile.cer (IMPORTANT: by default, this value is ignored because
-    ignoreselfsigned=True.  It is all or nothing.  Either you're ignoring certs or you
-    supply ones that aren't self signed.)`
+    `A path to a file containing one or multiple Certificate Authority signing
+    certificates. This value is ignored, by default, because ignoreselfsigned=True.
+    When ignoreselfsigned is true this client will ignore your proxy server's
+    certificate and implicitly trust it. Hence, a cafile is moot.
+    If you want to use this you need to set "-i" which will tell this client
+    to stop ignoring server certificates and you need to supply a
+    cafile that isn't self signed.`
+});
+parser.addArgument(["-s", "--capassphrase"], {
+  help:
+    `The secret for your encrypted CA file.
+    Please clear your shell history if you use this.`
 });
 
 let args = parser.parseArgs();
 
-
+let msg;
+if (args["proxy"]){
+  msg = `${chalk.green('INFO:')} Probing ${args["dest"]} through ${args["proxy"]}`;
+}else{
+  msg = `${chalk.yellow('WARNING:')} Probing ${args["dest"]} directly because you did not specify a
+proxy.  If you can reach ${args["dest"]} it means that your network isn't
+forcing you to egress traffic through a proxy.  This may or may not be
+desirable.`;
+}
+console.log(`${chalk.green('********************************************************************************')}`);
+console.log(msg);
+console.log(`${chalk.green('********************************************************************************')}\n`);
 probeURI(args["dest"],
          args["proxy"],
          args["ignoreselfsigned"],
